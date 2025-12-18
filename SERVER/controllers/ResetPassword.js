8a36213ecd4e8a2d3e3d850cd6cdce5f98bfb5ef
vrahul2108 <140827837+vrahul2108@users.noreply.github.com>
2025-12-18 22:37:16 +0530

    Adding Middleware and handlers

--- SERVER/controllers/ResetPassword.js
+++ SERVER/controllers/ResetPassword.js
@@ -1,5 +1,56 @@
 //reset Pass token
+const User = require("../models/User");
+const mailSender = require("../utils/mailSender");
+
 
 
 //reset pass
+exports.resetPasswordToken = async(req, res)=>{
+    try{
+        //extract email
+    const email = req.body.email;
 
+    //check user for this email, email verification
+    const user = await User.findOne({email: email});
+
+    if(!user){
+        return re.json({
+            success: false,
+            message: "Your email is not registered with us"
+        });
+    }
+
+    //gen token
+
+    const token = crypto.randomUUID();
+
+    //update user by adding token and expiration time
+    const updatedDetails = await User.findOneAndUpdate(
+        {email: email},
+        {
+            token : token,
+            resetPasswordExpires: Date.now()+ 5*60*1000,
+        },
+        {new: true}
+    );
+
+    //create url
+
+    const url = `http://localhost:3000/update-password/${token}`;
+    //send mail containing url
+
+    await mailSender(email, "Password Reset Link",`Password reset link: ${url}`);
+
+    //return response
+    return res.json({
+        success: true,
+        message: 'Reset password successfully'
+    });
+    }
+    catch(e){
+        return res.status(500).json({
+        success: false,
+        message: 'Something went wrong while reset password'
+    });
+    }
+}
--- SERVER/middleware/auth.js
+++ SERVER/middleware/auth.js
@@ -1,10 +1,100 @@
-//auth
+const jwt = require('jsonwebtoken');
+const user = require('../models/User');
+require('dotenv').config();
 
+//auth
+exports.auth = async(req, res, next) =>{
+    try{
+        //extract token
+        const token = req.body.token
+                      || req.cookies.token
+                      || req.header("Authorisation").replace("Bearer ","");
+
+        //If token missing
+        if(!token){
+            return res.status(401).json({
+                success: false,
+                message: "Token is missing"
+            });
+        }              
+
+        //verify the token 
+
+        try{
+            const decode = await jwt.verify(token, process.env.JWT_SECRET);
+            console.log(decode);
+            req.user = decode;
+            
+        }catch(e){
+            //verification issue
+           return res.status(401).json({
+            success: false,
+            message: "Token is Invalid"
+            }); 
+        }
+        next();
+    }catch(e){
+        console.log(e);
+        return res.status(500).json({
+            success: false,
+            message: "Internal Server Error"
+        });
+    }
+}
 
 //isStudent
-
+exports.isStudent = async(req, res, next)=>{
+    try{
+        if(req.user.accountType !== "isStudent"){
+        return res.status(401).json({
+            success: false,
+            message: "Protected route for student"
+        });
+        }
+        next();
+    }catch(e){
+        console.log(e);
+        return res.status(500).json({
+            success: false,
+            message: "Internal Server Error"
+        });
+    }
+}
 
 //isInstructor
-
+exports.isInstructor = async(req, res, next)=>{
+    try{
+        if(req.user.accountType !== "isInstructor"){
+        return res.status(401).json({
+            success: false,
+            message: "Protected route for Instructor"
+        });
+        }
+        next();
+    }catch(e){
+        console.log(e);
+        return res.status(500).json({
+            success: false,
+            message: "Internal Server Error"
+        });
+    }
+}
 
 //isAdmin
+exports.isAdmin = async(req, res, next)=>{
+    try{
+        if(req.user.accountType !== "isAdmin"){
+        return res.status(401).json({
+            success: false,
+            message: "Protected route for Instructor"
+        });
+        }
+        next();
+    }catch(e){
+        console.log(e);
+        return res.status(500).json({
+            success: false,
+            message: "Internal Server Error"
+        });
+    }
+}
