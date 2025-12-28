410149c2c170166633e0adffa7a5dfadf01d6f49
vrahul2108 <140827837+vrahul2108@users.noreply.github.com>
2025-12-28 22:10:25 +0530

    Section Handler added

---
+++ SERVER/controllers/Section.js
@@ -0,0 +1,96 @@
+const Section = require('../models/Section');
+const Course = require('../models/Course');
+
+exports.createSection = async(req, res)=>{
+    try{
+        const {sectionName, courseId} = req.body;
+
+        if(!sectionName || !courseId){
+            return res.status(400).json({
+                success: false,
+                message: 'Missing Properties',
+            });
+        }
+
+        const newSection = await Section.create({sectionName});
+
+        const updatedCourse = await Course.findByIdAndUpdate(
+            courseId,
+            {
+                $push: {
+                    courseContent: NewSection._id,
+                }
+            },
+            {new: true},
+        )
+        .populate({
+        path: "courseContent",
+        populate: {
+            path: "subSection",
+        },
+        });
+        return res.status(200).json({
+                success: true,
+                message: 'Section created successsfully',
+                updatedCourse,
+            });
+    }catch(e){
+        console.log(e);
+        return res.status(500).json({
+                success: false,
+                message: 'Internal Server Error',
+            });
+    }
+};
+
+exports.updateCourse = async(req, res)=>{
+    try{
+        const {sectionName, sectionId} = req.body;
+
+        if(!sectionName || !sectionId){
+            return res.status(400).json({
+                success: false,
+                message: 'Unable to fetch Data',
+            });
+        }
+
+        //update data
+        const section = await Section.findByIdAndUpdate(
+            sectionId,
+            {sectionName},
+            {new:true}
+        )
+
+        return res.status(200).json({
+                success: true,
+                message: 'Section updated successfully',
+            });
+        
+    }catch(e){
+        console.log(e);
+        return res.status(500).json({
+                success: false,
+                message: 'Internal Server Error',
+            });
+    }
+};
+
+exports.deleteSection = async (req, res)=>{
+    try{
+        const {sectionId} = req.params;
+
+        await Section.findByIdAndDelete({sectionId});
+
+        return res.status(200).json({
+                success: true,
+                message: 'Section deleted successfully',
+            });
+
+    }catch(e){
+        console.log(e);
+        return res.status(500).json({
+                success: false,
+                message: 'Internal Server Error',
+            });
+    }
+}
